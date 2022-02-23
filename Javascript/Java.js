173ee004b5f9827f358ce058b809c7c945f17fd7
Saraphina <saraphinashanell@gmail.com>
2022-02-23 15:04:13 +0200

    Updates

---
+++ Javascript/Java.js
@@ -0,0 +1,9 @@
+document.getElementById('contact1').innerHTML="For any enquries please email us and leave a message, call" +
+"<br>or find us on social media platforms.";
+document.getElementById('heading2').innerHTML="Email Us";
+document.getElementById('contact2').innerHTML="Thank you for your input. We will keep in touch with you.";
+document.getElementById('contact3').innerHTML=" <b>We are based in:</b><br>" +
+"VandernijlPark<br>" +
+"Andries Potgieter blvd<br>" +
+"Vaal University of Technology<br>"+
+"1900 <br>";
